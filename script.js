840eedee8a61147e1cb1d70963144415484d98fa
dnnn3399 <dawnnguy3n@gmail.com>
2025-10-16 10:25:09 -0400

    added script

---
+++ script.js
@@ -0,0 +1,10 @@
+const choices = ["X", "O",];
+const player1Display = document.getElementById("player1Display");
+const Player2Display = document.getElementById("player2Display");
+const resultDisplay = document.getElementById("resultDisplay");
+const player1ScoreDisplay = document.getElementById("player1ScoreDisplay");
+const Player2ScoreDisplay = document.getElementById("Player2ScoreDisplay");
+const tieScoreDisplay = document.getElementById("tieScoreDisplay");
+let player1Score = 0;
+let Player2Score = 0;
+let tieScore = 0;
